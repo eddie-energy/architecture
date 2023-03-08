97ca4f456aae95a85eeaeeb7eddcafdb2496e47e
Henner Carl <carl@ponton.de>
2023-03-08 23:18:29 +0100

    REF using yaml instead of JSON for data files bc. they can have comments

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
+const yaml = require("js-yaml");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.setDataFileBaseName("index");
   eleventyConfig.ignores.add("README.md");
