60f7e48a39c98bae6f49a578d81870c260d27a2e
Henner Carl <carl@ponton.de>
2023-03-22 10:25:47 +0100

    IMP added subfolder navigation

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,4 +1,6 @@
-const getOrderNumberRegex = /^\/(\d+)-/;
+const dirname = require("path").posix.dirname;
+
+const getOrderNumberRegex = /\/(\d+)[^\/]*\/$/;
 
 module.exports = {
   eleventyNavigation: {
@@ -8,6 +10,15 @@ module.exports = {
       const match = data.page.url.match(getOrderNumberRegex);
       return match ? match[1] : -1;
     },
-    parent: (data) => data.page.url==='/' ? undefined : '/'
+    parent: (data) => {
+      const parentPath = dirname(data.page.url);
+      let newResult = parentPath + "/";
+      if (data.page.url === "/") {
+        newResult = undefined;
+      } else if ((parentPath === ".") | (parentPath === "/")) {
+        newResult = "/";
+      }
+      return newResult;
+    },
   },
 };
