7c24028b8d3a4463fcc320610176705fde55bd4f
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2023-11-08 09:43:07 +0100

    NOISSUE Adapt to new folder structure

--- bprt/src/config.ts
+++ bprt/src/config.ts
@@ -1,5 +1,5 @@
 export default {
-  "AT": "./arc42/05-building-block-view/development/dev-eda/issues.md",
-  "FR": "./arc42/05-building-block-view/development/dev-enedis/issues.md",
-  "ES": "./arc42/05-building-block-view/development/dev-datadis/issues.md"
+  "AT": "./arc42/05-building-block-view/development/development/dev-eda/issues.md",
+  "FR": "./arc42/05-building-block-view/development/development/dev-enedis/issues.md",
+  "ES": "./arc42/05-building-block-view/development/development/dev-datadis/issues.md"
 }
