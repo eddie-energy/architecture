ad1b793534e5c9b6311221309c3df140552df6f0
Henner Carl <carl@ponton.de>
2024-07-01 10:38:55 +0200

    NOISSUE Remove 11ty and bprt code

--- bprt/src/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  "AT": "./arc42/building-block-view/regional-hurdles/dev-eda/issues.md",
-  "FR": "./arc42/building-block-view/regional-hurdles/dev-enedis/issues.md",
-  "ES": "./arc42/building-block-view/regional-hurdles/dev-datadis/issues.md"
-}
--- eleventy/scripts/structurizr-iframe-sync.js
+++
@@ -1,6 +0,0 @@
-function receiveStructurizrMessage(message) {
-    if (message?.data?.view)
-        console.log("diagram navigated", { "new-diagram-key": message.data.view })
-}
-
-window.addEventListener("message", receiveStructurizrMessage);
