fd302c1d2218ec35c955925530bab2554bfe61cd
bemayr <bernhard@mayr.io>
2024-07-11 12:20:17 +0200

    NOISSUE feat: improve C4 diagram handling
    - while merging from 11ty to vitepress some of C4's functionality got lost, this commit restores these features

--- .vitepress/theme/C4.vue
+++ .vitepress/theme/C4.vue
@@ -1,16 +1,40 @@
 <script lang="ts" setup>
+const props = defineProps<{ diagram: string }>();
+let diagramKey = props.diagram;
 
-const props = defineProps<{diagram: string}>();
-const diagram = props.diagram;
+// prevent users form accidentally prefixing the diagramKey with '#'
+if (diagramKey.startsWith("#")) {
+  console.warn(
+    `WARNING (${diagramKey}): please do not prefix the diagram key with '#'`
+  );
+  diagramKey = diagramKey.substring(1);
+}
 
-const id="c4_eddie_someCrazyIdThingy"
-const src=`https://diagrams-eddie.projekte.fh-hagenberg.at/embed/1?diagram=${diagram}&amp;diagramSelector=false&amp;iframe=${id}`;
+// generate random suffix so that ids do not collide (https://stackoverflow.com/a/33146982)
+const suffix = btoa(String(Math.random())).slice(-7, -2);
+const id = `c4_${diagramKey}_${suffix}`;
+
+// TODO: read from environment variables, so that local builds are possible as well
+// reference: https://vitejs.dev/guide/env-and-mode
+// how it was: https://github.com/eddie-energy/architecture/pull/59/files#diff-88644e2fe55cfcc9d38322f85653f8a3ee472c5bf5cc477c0e0d6e81b0209733
+const structurizrWorkspaceId = 1;
+const structurizrBasePath = "https://diagrams-eddie.projekte.fh-hagenberg.at";
+
+// create the URL using JavaScript's URL API (this allows us to properly append the pathname and search params)
+const src = new URL(structurizrBasePath);
+src.pathname = `embed/${structurizrWorkspaceId}`;
+src.searchParams.append("diagram", diagramKey);
+src.searchParams.append("diagramSelector", String(false));
+src.searchParams.append("iframe", id);
 </script>
 
 <style>
 .c4-diagram {
-    border-top: 1px solid var(--vp-c-divider);
-    border-bottom: 1px solid var(--vp-c-divider);
+  border-top: 1px solid var(--vp-c-divider);
+  border-bottom: 1px solid var(--vp-c-divider);
+  width: 100%;
+  background: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100% 100%"><text fill="%23333333" x="50%" y="50%" font-family="\'Lucida Grande\', sans-serif" font-size="24" text-anchor="middle">loading...</text></svg>')
+    0px 0px no-repeat;
 }
 </style>
 
@@ -18,14 +42,12 @@ const src=`https://diagrams-eddie.projekte.fh-hagenberg.at/embed/1?diagram=${dia
   <iframe
     class="c4-diagram"
     :id
-    :src
-    width="1152px"
+    :src="src.href"
     marginwidth="0"
     marginheight="0"
     frameborder="0"
     scrolling="no"
     allowfullscreen="true"
-    height="929px"
   >
   </iframe>
 </template>
