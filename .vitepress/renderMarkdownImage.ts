955904ab7b5c302f4f28f950c3f562e23f431342
bemayr <bernhard@mayr.io>
2024-08-22 12:14:54 +0200

    debug duplicate image bug
    - added logging statements for the parameters of the markdown-it image override function
    - wrapped the <Img> element inside a <ClientOnly> component which fixes the issue
    - fixes #74

--- .vitepress/renderMarkdownImage.ts
+++ .vitepress/renderMarkdownImage.ts
@@ -1,7 +1,12 @@
-
 const renderMarkdownImage = (tokens, idx, _options, _env, _self) => {
-    const token = tokens[idx];
-    return `<Img src="${token.attrGet("src")}" alt="${token.attrGet("alt")}" />`
-  };
+  const token = tokens[idx];
+  const src = token.attrGet("src");
+  const alt = token.attrGet("alt");
+  if (src.includes("aiida_deployment_view")) {
+    console.log({ tokens, idx, _options, _env, _self });
+    console.log({ token, src, alt });
+  }
+  return `<ClientOnly><Img src="${src}" alt="${alt}" /></ClientOnly>`;
+};
 
 export default renderMarkdownImage;
